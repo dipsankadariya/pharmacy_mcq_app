289fd25227e7ab420ba6b6f4a2922dde253dbab6
Aiswarya Pokharel <pokharelaishwarya22@gmail.com>
2025-02-10 21:20:06 +0545

    new

---
+++ lib/firebase_services/formContainer.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+class FormContainer extends StatefulWidget {
+  final TextEditingController? controller;
+  final Key? fieldkey;
+  final bool? isPasswordField;
+  final String? hintText;
+  final String? helperText;
+  final String? labelText;
+  final FormFieldSetter<String>? onsaved;
+  final FormFieldValidator<String>? validator;
+  final TextInputType? inputType;
+  final ValueChanged<String>? onfieldSubmitted;
+
+  const FormContainer(
+      {this.controller,
+      this.fieldkey,
+      this.helperText,
+      this.hintText,
+      this.inputType,
+      this.isPasswordField,
+      this.labelText,
+      this.onfieldSubmitted,
+      this.onsaved,
+      this.validator});
+
+  @override
+  _FormContainerState createState() => _FormContainerState();
+}
+
+class _FormContainerState extends State<FormContainer> {
+  bool _obsecure = true;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+          color: Colors.grey.shade100, borderRadius: BorderRadius.circular(10)),
+      child: TextFormField(
+        controller: widget.controller,
+        key: widget.fieldkey,
+        keyboardType: widget.inputType,
+        validator: widget.validator,
+        onFieldSubmitted: widget.onfieldSubmitted,
+        obscureText: widget.isPasswordField == true ? _obsecure : false,
+        decoration: InputDecoration(
+            border: InputBorder.none,
+            hintText: widget.hintText,
+            hintStyle: const TextStyle(color: Colors.black),
+            suffixIcon: widget.isPasswordField == true
+                ? GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        _obsecure = !_obsecure;
+                      });
+                    },
+                    child: Icon(
+                      _obsecure ? Icons.visibility_off : Icons.visibility,
+                      color: _obsecure == false ? Colors.blue : Colors.grey,
+                    ),
+                  )
+                : null),
+      ),
+    );
+  }
+}
---
+++ lib/pages/sign_in.dart
@@ -0,0 +1,103 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:mcq_app/firebase_services/authentication.dart';
+import 'package:mcq_app/firebase_services/formContainer.dart';
+import 'package:mcq_app/pages/sign_up.dart';
+
+class SignInPage extends StatefulWidget {
+  const SignInPage({super.key});
+
+  @override
+  State<SignInPage> createState() => _SignInPageState();
+}
+
+class _SignInPageState extends State<SignInPage> {
+  final FirebaseAuthService _auth = FirebaseAuthService();
+  TextEditingController _emailController = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Center(
+              child: Text(
+            'Sign In',
+            style: TextStyle(fontSize: 25),
+          )),
+        ),
+        body: Center(
+          child: Column(
+            children: [
+              const Text('Log In'),
+              const SizedBox(height: 30),
+              FormContainer(
+                controller: _emailController,
+                hintText: 'Email',
+                isPasswordField: false,
+              ),
+              const SizedBox(height: 30),
+              FormContainer(
+                controller: _passwordController,
+                hintText: 'Password',
+                isPasswordField: true,
+              ),
+              const SizedBox(height: 30),
+              GestureDetector(
+                  child: Container(
+                      width: 200,
+                      height: 40,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color: Colors.blue),
+                      child: const Center(
+                          child: Text(
+                        'Log In',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 25),
+                      )))),
+              const SizedBox(height: 18),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text("Don't you have an account?"),
+                  const SizedBox(width: 2),
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.pushAndRemoveUntil(
+                          context,
+                          MaterialPageRoute(builder: (context) => SignupPage()),
+                          (route) => false);
+                    },
+                    child: const Text(
+                      "Sign Up",
+                      style: TextStyle(
+                        color: Colors.blue,
+                      ),
+                    ),
+                  )
+                ],
+              )
+            ],
+          ),
+        ));
+  }
+
+  void _signIn() async {
+    String email = _emailController.text;
+    String password = _passwordController.text;
+    User? user = await _auth.signInWithEmailAndPassword(email, password);
+
+    if (user != null) {
+    } else {}
+  }
+}
