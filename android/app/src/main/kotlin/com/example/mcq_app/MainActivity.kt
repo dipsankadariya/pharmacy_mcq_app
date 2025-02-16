d8b63ae18fce7071852247cd57f2f5400ef8756b
Dipsan Kadariya <dipsankadariya99@gmail.com>
2025-02-16 18:19:07 +0545

    changes

--- android/app/src/main/kotlin/com/example/mcq_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/mcq_app/MainActivity.kt
@@ -2,4 +2,6 @@ package com.example.mcq_app
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterActivity() {
+    // No need to override configureFlutterEngine unless you have custom plugins
+}
